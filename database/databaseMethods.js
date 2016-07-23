8363cf66535578fcbb85d570aeefebcb760133ff
Steven Harlan <SteveHarlan@Stevens-MacBook-Air.local>
2016-07-22 21:58:21 -0700

    moved database folder

---
+++ database/databaseMethods.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+var db = mongoose.connection;
+
+var host = require('../Database/Models/HostModel');
+var poll = require('../Database/Models/PollModel');
+//verifying
+db.on('error', console.error);
+db.once('open', function() {
+  console.log('open!');
+});
+
+var dbMethods = {};
+
+// Mongodb CRUD Operations
+
+dbMethods.savePollInstance = function(pollToSave){
+  var pollTemp = new poll(pollToSave);
+  pollTemp.save(function(err, pollToSave) {
+    if (err) return console.error('Error! ' + err);
+    console.dir('saved!');
+  });
+}
+
+dbMethods.deletePollInstance = function(pollToDelete_id){
+  poll.findByIdAndRemove(pollToDelete_id, function(err, poll){
+    console.log('removed ' + poll);
+  })
+}
+
+
+
+
+
+
+
+
+mongoose.connect('mongodb://localhost/tadpoll');
+
+module.exports = dbMethods;
