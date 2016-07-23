69c8d445efa923430b87bc00e4a3c21c06134155
Sarah Ford <sford85@gmail.com>
2016-07-22 17:38:06 -0700

    base front end code

--- components/App.js
+++ components/App.js
@@ -1,18 +1,44 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var LoginBox = require('./LoginBox');
 
-var App = React.createClass({
-  render: function () {
+
+// import 'normalize.css/normalize.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+import { Grid, Row, Col } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import LoginBox from './LoginBox';
+
+
+export default class App extends Component {
+  render() {
     return (
       <div id='App'>
-        App
         <LoginBox />
       </div>
     )
   }
-});
+}
 
-module.exports = App;
+render(<App />, document.getElementById('main-container'));
 
-ReactDOM.render(<App />, document.getElementById('main-container'));
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var LoginBox = require('./LoginBox');
+//
+//
+// var App = React.createClass({
+//   render: function () {
+//     return (
+//       <div id='App'>
+//         App
+//         <LoginBox />
+//       </div>
+//     )
+//   }
+// });
+//
+// module.exports = App;
+
+
+
+// ReactDOM.render(<App />, document.getElementById('main-container'));
--- components/LoginBox.js
+++ components/LoginBox.js
@@ -1,16 +1,65 @@
-var React = require('react');
+import { FormGroup, FormControl, Well } from 'react-bootstrap';
+import React, {Component} from 'react';
 
-
-var LoginBox = React.createClass({
-  render: function () {
+export default class LoginBox extends Component {
+  render() {
     return (
       <div id='LoginBox'>
-        LoginBox
+        <form>
+        <Well>
+        
+           <FormGroup controlId="formBasicText">
+             <FormControl
+               type="text"
+              // value={this.state.value}
+               placeholder="Username"
+              // onChange={this.handleChange}
+             />
+             <FormControl.Feedback />
+             <FormControl
+               type="text"
+              // value={this.state.value}
+               placeholder="Password"
+              // onChange={this.handleChange}
+             />
+             <FormControl.Feedback />
+           </FormGroup>
+           </Well>
+         </form>
+
       </div>
     )
   }
-});
+}
 
-module.exports = LoginBox;
+// render(<App />, document.getElementById('main-container'));
 
-
+// var React = require('react');
+//
+// // import 'normalize.css/normalize.css';
+// // import 'bootstrap/dist/css/bootstrap.min.css';
+// // import React, { Component } from 'react';
+// // // import Header from './Header';
+// // // import Sidebar from './Sidebar';
+// // import { Grid, Row, Col } from 'react-bootstrap';
+// var Button = require('react-bootstrap/lib/Button');
+//
+//
+// var LoginBox = React.createClass({
+//   render: function () {
+//     return (
+//       <div id='LoginBox'>
+//         LoginBox
+//         <input type='text' placeholder='Username' />
+//         <Button bsStyle="primary" bsSize="large" active>Primary button</Button>
+//         {/*<div className='form-group'>
+//           <input type='text' placeholder='Username' />
+//           <input type='password' placeholder='Password' />
+//         </div>
+//         <button type='submit' onClick={this.login.bind(this)}>Submit</button>*/}
+//       </div>
+//     )
+//   }
+// });
+//
+// module.exports = LoginBox;
